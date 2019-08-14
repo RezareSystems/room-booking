5f35dc420fb161b4f267c1754f9bc5f54a71a359
andre-wilson40 <andre.wilson40@gmail.com>
2019-08-14 15:26:08 +1200

    Empty project for lambda solution

---
+++ Rezare.OpenMeeting/Rezare.OpenMeeting.Application/Function.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace Rezare.OpenMeeting.Application
+{
+    public class Function
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Function"/> class.
+        /// 
+        /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
+        /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
+        /// region the Lambda function is executed in.
+        /// </summary>
+        public Function()
+        {
+        }
+
+        public APIGatewayProxyResponse GetBookings(
+            APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Body = JsonConvert.SerializeObject(HttpStatusCode.OK),
+                Headers = new Dictionary<string, string>
+                { 
+                    { "Content-Type", "application/json" }, 
+                    { "Access-Control-Allow-Origin", "*" } 
+                }
+            };;
+        }
+    }
+}
